19021d6975910bd5d58a6f667750a81b15e63885
Jon Packer <contact@jonpacker.com>
2019-05-28 16:27:17 +0200

    i am a great coder 🤠

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var Component = React.Component
+
+class LoginForm extends Component {
+  constructor (props) {
+    super(props);
+    this.state = { }
+  }
+
+  render() {
+    var component = (
+      <form className="login-form">
+        <div className="login-username">
+          <b className="login-label">Username:</b>
+          <input type="text" onChange={e => this.setState({ username: e.target.value })} value={this.state.username} />
+        </div>
+        <div className="login-password-label">
+          <b className="login-password">Password:</b>
+          <div className="login-showpass">Show</div>
+          <input type='checkbox' checked={this.state.showpass} onChange={() => this.setState({ showpass: !this.state.showpass })} />
+        </div>
+        <input type={this.state.showpass ? 'text' : 'password'} value={this.state.password} onChange={e => this.setState({ password: e.target.value })}/>
+        <input type="button" onClick={() => alert('Not Implemented 🦙a')} value="Log In" />
+      </form>
+    )
+    this.state = {
+      username: '',
+      password: '',
+      showpass: false
+    }
+    return component
+  }
 }
 
-export default App;
+export default LoginForm
+
